7d33f4975833f5aa5f2e612cacce70eb785f6eac
SrMetus <s.gaggero87@gmail.com>
2023-12-28 03:35:15 -0300

    info pokemon

--- pokedex/src/store/flux.js
+++ pokedex/src/store/flux.js
@@ -1,18 +1,18 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	const url = "https://pokeapi.co/api/v2/pokemon/1";
+	//const url = "https://pokeapi.co/api/v2/pokemon/";
 	return {
 		store: {
 			pokemons: [],
 			pokemon: {}
 		},
 		actions: {
-			getPokemons: async () => {
+			getPokemons: async (id) => {
 				try {
-					const resp = await fetch(url);
+					const resp = await fetch(`https://pokeapi.co/api/v2/pokemon/1`);
 					const data = await resp.json();
 					console.log("data", data)
 
-					setStore({ pokemons: data.resp });
+					setStore({ pokemons: data });
 				} catch (error) {
 					console.log("Error al obtener los Pokémon", error);
 				}
